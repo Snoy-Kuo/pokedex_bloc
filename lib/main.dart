bfd59205f8394d8c3cf72fea36af30866e92e7d2
snoykuo <snoy.kuo@twprotech.com>
2021-04-28 12:19:18 +0800

    add win support.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // @dart=2.9
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:window_size/window_size.dart';
 
 import 'app.dart';
 import 'common/repository/remote_repository.dart';
@@ -9,6 +12,13 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   Bloc.observer = BlocObserver();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Pokedex Bloc');
+    setWindowMinSize(const Size(375, 667)); //iPhone 1x
+    setWindowMaxSize(Size.infinite);
+  }
+
   runApp(App(
     repository: RemoteRepository(),
   ));
